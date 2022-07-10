24eda64baec5bc44d950228088babef785e8afa7
Kevin Brice <kevinbrice@me.com>
2022-07-09 19:48:34 -0700

    Upload Version Structure for OTA

---
+++ FW-Workspace/backUp-1.0/src/OTA.h
@@ -0,0 +1,152 @@
+//GET direct download link for github
+//https://gist.github.com/fedarko/4fc177cff9084b9e325dcbe954547edc
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
+#include <WiFiClientSecure.h>
+#include <cert.h>
+
+//https://github.com/inteliqoiq/ESP32_OTA/blob/main/firmware.bin
+String FirmwareVer = {
+  "1.9"// While loop eleminated from wifi connect. Onboard wifi indication
+};
+#define URL_fw_Version "https://raw.githubusercontent.com/inteliqoiq/ESP32_OTA/master/version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/inteliqoiq/ESP32_OTA/master/firmware.bin"
+
+
+
+//#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
+//#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
+
+void connect_wifi();
+void firmwareUpdate();
+int FirmwareVersionCheck();
+
+
+
+void CHK_FIRMWARE() {
+
+
+    // save the last time you blinked the LED
+
+    if (FirmwareVersionCheck()) {
+      firmwareUpdate();
+    }
+  }
+
+ 
+ 
+  
+
+
+struct Button {
+  const uint8_t PIN;
+  uint32_t numberKeyPresses;
+  bool pressed;
+};
+
+Button button_boot = {
+  0,
+  0,
+  false
+};
+/*void IRAM_ATTR isr(void* arg) {
+    Button* s = static_cast<Button*>(arg);
+    s->numberKeyPresses += 1;
+    s->pressed = true;
+}*/
+
+void IRAM_ATTR isr() {
+  button_boot.numberKeyPresses += 1;
+  button_boot.pressed = true;
+}
+
+
+
+
+void setupOTA() {
+
+  Serial.print("Active firmware version:");
+  Serial.println(FirmwareVer);
+  pinMode(2, OUTPUT);
+}
+
+
+
+
+
+
+
+
+void firmwareUpdate(void) {
+  WiFiClientSecure client;
+  client.setCACert(rootCACertificate);
+  httpUpdate.setLedPin(2, LOW);
+  t_httpUpdate_return ret = httpUpdate.update(client, URL_fw_Bin);
+
+  switch (ret) {
+  case HTTP_UPDATE_FAILED:
+    Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+    break;
+
+  case HTTP_UPDATE_NO_UPDATES:
+    Serial.println("HTTP_UPDATE_NO_UPDATES");
+    break;
+
+  case HTTP_UPDATE_OK:
+    Serial.println("HTTP_UPDATE_OK");
+    break;
+  }
+}
+int FirmwareVersionCheck(void) {
+  String payload;
+  int httpCode;
+  String fwurl = "";
+  fwurl += URL_fw_Version;
+  fwurl += "?";
+  fwurl += String(rand());
+  Serial.println(fwurl);
+  WiFiClientSecure * client = new WiFiClientSecure;
+
+  if (client) 
+  {
+    client -> setCACert(rootCACertificate);
+
+    // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+    HTTPClient https;
+
+    if (https.begin( * client, fwurl)) 
+    { // HTTPS      
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      delay(100);
+      httpCode = https.GET();
+      delay(100);
+      if (httpCode == HTTP_CODE_OK) // if version received
+      {
+        payload = https.getString(); // save received version
+      } else {
+        Serial.print("error in downloading version file:");
+        Serial.println(httpCode);
+      }
+      https.end();
+    }
+    delete client;
+  }
+      
+  if (httpCode == HTTP_CODE_OK) // if version received
+  {
+    payload.trim();
+    if (payload.equals(FirmwareVer)) {
+      Serial.printf("\nDevice already on latest firmware version:%s\n", FirmwareVer);
+      return 0;
+    } 
+    else 
+    {
+      Serial.println(payload);
+      Serial.println("New firmware detected");
+      return 1;
+    }
+  } 
+  return 0;  
+}
